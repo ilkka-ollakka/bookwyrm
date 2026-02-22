db382a3293c519001ab7821ba08907ebb8788e42
Ilkka Ollakka <ilkka.ollakka@iki.fi>
2026-02-22 11:37:21 +0200

    postgres-docker: use zstd compression on backups

--- postgres-docker/backup.sh
+++ postgres-docker/backup.sh
@@ -10,5 +10,5 @@ fi
 BACKUP_DB=${POSTGRES_DB:-bookwyrm}
 BACKUP_USER=${POSTGRES_USER:-bookwyrm}
 filename=backup_${BACKUP_DB}_$(date +%F)
-pg_dump -U "${BACKUP_USER}" "${BACKUP_DB}" --file "/backups/$filename.sql"
+pg_dump --compress=zstd -U "${BACKUP_USER}" "${BACKUP_DB}" --file "/backups/$filename.sql"
 info "backup: completed backup of $BACKUP_DB to /backups/$filename.sql"
